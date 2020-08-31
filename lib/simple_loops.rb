b7ecc4c595c669cdd9257ed3c5c8e7e36698be89
Stacey P Carrillo <staceycarrillo44@gmail.com>
2020-08-31 23:33:50 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,21 @@
-# Write your methods here
+def loop_message_five_times(string)
+  counter = 0
+  while counter <= 5
+    puts(string)
+  end
+end
+message = "Hello World."
+loop_message_five_times(message)
+
+def loop_message_n_times
+  
+end
+
+def output_array
+  
+end
+
+def return_string_array
+  
+end
+
